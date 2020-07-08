b3dd1ed148b86919fc8932104f45737ced1a52a1
Abhishek Gupta <guptaabhishek891@gmail.com>
2020-07-08 19:57:35 +0530

    prototype Classes added

--- firstapplication/src/main/java/com/example/firstapplication/FirstApplication.java
+++ firstapplication/src/main/java/com/example/firstapplication/FirstApplication.java
@@ -3,6 +3,7 @@ package com.example.firstapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.applet.AppletContext;
 
@@ -12,14 +13,19 @@ public class FirstApplication {
     public static void main(String[] args) {
 
 //        SpringApplication.run(FirstApplication.class, args);
-        ApplicationContext context=SpringApplication.run(FirstApplication.class,args);
-        Alien obj=context.getBean(Alien.class);
+//        ApplicationContext context=SpringApplication.run(FirstApplication.class,args);
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+//      type conversion to alien type
+        Alien obj=(Alien)context.getBean("alien");
         obj.time=1;
         System.out.println(obj.time);
         obj.code();
 //        in this case both the objects are pointing to a single object
-//        for eg: time for 2nd object is not initialized still it shows value as 1
+//        for eg: time for 2nd object is not initialized  still it shows value as 1
 //        this is called singleTon classes
+
+//        this is prototype(multiple objects)
+        //      type conversion not needed alien class specified
         Alien obj1=context.getBean(Alien.class);
 //        obj.time=1;
         System.out.println(obj1.time);
