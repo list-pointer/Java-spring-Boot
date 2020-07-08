a64844ea617eb7e28737f99c4186b390a1a00876
Abhishek Gupta <guptaabhishek891@gmail.com>
2020-07-08 19:28:39 +0530

    Autowired added

--- firstapplication/src/main/java/com/example/firstapplication/Alien.java
+++ firstapplication/src/main/java/com/example/firstapplication/Alien.java
@@ -1,10 +1,14 @@
 package com.example.firstapplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
+    laptop lap;
+    @Autowired
     public void code(){
-        System.out.println("Let's code.....");
+//        System.out.println("Let's code.....");
+        lap.run();
     }
 }
---
+++ firstapplication/src/main/java/com/example/firstapplication/laptop.java
@@ -0,0 +1,10 @@
+package com.example.firstapplication;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class laptop {
+    public void run() {
+        System.out.println("runnning.....");
+    }
+}
