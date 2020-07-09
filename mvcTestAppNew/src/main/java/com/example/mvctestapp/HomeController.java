21fa6afac647f9614eb50c8ee8b85c7836c159a8
Abhishek Gupta <guptaabhishek891@gmail.com>
2020-07-09 14:32:37 +0530

    basic user input and output updated

--- mvcTestAppNew/src/main/java/com/example/mvctestapp/HomeController.java
+++ mvcTestAppNew/src/main/java/com/example/mvctestapp/HomeController.java
@@ -2,6 +2,7 @@ package com.example.mvctestapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -16,12 +17,12 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(HttpServletRequest request)
+    public String add(@RequestParam("num1")int i,@RequestParam("num2")int j, HttpSession session)
     {
-        int i=Integer.parseInt(request.getParameter("num1"));
-        int j=Integer.parseInt(request.getParameter("num2"));
+//        int i=Integer.parseInt(request.getParameter("num1"));
+//        int j=Integer.parseInt(request.getParameter("num2"));
         int sum=i+j;
-        HttpSession session=request.getSession();
+//        HttpSession session=request.getSession();
         session.setAttribute("sum",sum);
         return "result.jsp";
     }
