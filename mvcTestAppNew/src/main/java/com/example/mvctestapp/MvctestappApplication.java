e9c5096a6c6b0e71da5ac2ed1a609367d105f267
Abhishek Gupta <guptaabhishek891@gmail.com>
2020-07-09 12:53:21 +0530

    basic spring boot mvc project initalized

---
+++ mvcTestAppNew/src/main/java/com/example/mvctestapp/MvctestappApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvctestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvctestappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvctestappApplication.class, args);
+    }
+
+}
---
+++ mvcTestAppNew/src/test/java/com/example/mvctestapp/MvctestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvctestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvctestappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
