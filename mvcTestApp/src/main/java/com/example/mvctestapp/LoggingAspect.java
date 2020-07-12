e3294cb51d000661c2d4f275e80c13e3e116c235
Abhishek Gupta <guptaabhishek891@gmail.com>
2020-07-12 17:37:30 +0530

    added log

--- mvcTestApp/src/main/java/com/example/mvctestapp/LoggingAspect.java
+++ mvcTestApp/src/main/java/com/example/mvctestapp/LoggingAspect.java
@@ -1,5 +1,7 @@
 package com.example.mvctestapp;
 
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -7,9 +9,11 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingAspect {
+    private static final Logger logger = LoggerFactory.getLogger((LoggingAspect.class));
     @Before(value = "execution(public * com.example.mvctestapp.HomeController.getAliens())")
     public void log()
     {
-        System.out.println("Calling get Aliens");
+//        System.out.println("Calling get Aliens");
+        logger.trace("Logger Called for getAliens");
     }
 }
