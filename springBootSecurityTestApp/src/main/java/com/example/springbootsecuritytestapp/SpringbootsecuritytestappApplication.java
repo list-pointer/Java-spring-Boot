1f80b5ccaf36656bb36b6a229d4d38041b02d153
Abhishek Gupta <guptaabhishek891@gmail.com>
2020-07-12 18:15:14 +0530

    spring security project initialized

---
+++ springBootSecurityTestApp/src/main/java/com/example/springbootsecuritytestapp/SpringbootsecuritytestappApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecuritytestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsecuritytestappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootsecuritytestappApplication.class, args);
+    }
+
+}
---
+++ springBootSecurityTestApp/src/test/java/com/example/springbootsecuritytestapp/SpringbootsecuritytestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecuritytestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsecuritytestappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
