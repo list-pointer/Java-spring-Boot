8605ee06060db8f2ffa16a1d0a65d7d5e48d5ef6
Abhishek Gupta <guptaabhishek891@gmail.com>
2020-07-16 14:10:29 +0530

    REST Controller added PostGetting
    Signed-off-by: Abhishek Gupta <guptaabhishek891@gmail.com>

--- mvcTestApp/src/main/java/com/example/mvctestapp/ApiController.java
+++ mvcTestApp/src/main/java/com/example/mvctestapp/ApiController.java
@@ -3,6 +3,7 @@ package com.example.mvctestapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,6 +24,12 @@ public class ApiController {
     @GetMapping("/Alien/{id}")
     public infotable getAlien(@PathVariable("id") int id) {
         infotable info = infotablerepo.findById(id).orElse(new infotable());
+        return info;
+    }
+
+    @PostMapping("Alien")
+    public infotable sendAlien(infotable info) {
+        infotablerepo.save(info);
         return info;
     }
 }
